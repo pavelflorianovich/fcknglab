6b114edaeef1eb70a536ef79f66fb53ed084f8d4
ma1evo1ence <55546948+ma1evo1ence@users.noreply.github.com>
2019-09-20 01:37:45 +0300

    Add files via upload

---
+++ privalov.py
@@ -0,0 +1,18 @@
+from graph import *
+brushcolor(135,206,250)
+rectangle(0,0,500,1000)
+brushColor(218,165,32)
+rectangle(0,130,500,360)
+brushColor(60,179,113)
+rectangle(0,360,500,1000)
+def fence:
+    for i in range (x):
+        brushColor(218,165,32)
+        rectangle(33*i,130,33*(i+1),360)
+fence(15)
+brushColor(238,180,40)
+polygon([(275,370),(385,410),(425,370),(370,270),325,294)])
+line(385,410,325,294)
+brushColor("black")
+circle(310,460,23)
+run()
